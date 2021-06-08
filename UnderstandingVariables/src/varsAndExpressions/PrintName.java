c3a584f937504e5f2459ab0f09977ad519a5c097
Ekta Choudhary <ekta.choudhary23@gmail.com>
2021-06-08 14:35:46 +0530

    Project for Variables and Expressions

---
+++ UnderstandingVariables/src/varsAndExpressions/PrintName.java
@@ -0,0 +1,11 @@
+package varsAndExpressions;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Harshada Dhone");
+	}
+
+}
